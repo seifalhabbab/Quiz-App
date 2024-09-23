d6bc663a2878ce5f348ae903543e283860e7de91
Seif A. Al-Habbab <93404965+seifalhabbab@users.noreply.github.com>
2024-09-24 01:06:18 +0300

    Complete Project

---
+++ script.js
@@ -0,0 +1,121 @@
+const questions = [
+  {
+    question: "Which language runs in a web browser?",
+    answers: [
+      { text: "Java", correct: false },
+      { text: "C", correct: false },
+      { text: "Python", correct: true },
+      { text: "JavaScript", correct: false },
+    ],
+  },
+  {
+    question: "What does CSS stand for?",
+    answers: [
+      { text: "Central Style Sheets", correct: false },
+      { text: "Cascading Style Sheets", correct: true },
+      { text: "Cascading Simple Sheets", correct: false },
+      { text: "Cars SUVs Style Sheets", correct: false },
+    ],
+  },
+  {
+    question: "What does HTML stand for?",
+    answers: [
+      { text: "Hypertext Markup Language", correct: true },
+      { text: "Hypertext Markdown Language", correct: false },
+      { text: "Hyperloop Machine Language", correct: false },
+      { text: "Helicopters Terminals Motorboats Lamborginis", correct: false },
+    ],
+  },
+  {
+    question: "What year was JavaScript launched?",
+    answers: [
+      { text: "1996", correct: true },
+      { text: "1995", correct: false },
+      { text: "1994", correct: false },
+      { text: "none of the above", correct: false },
+    ],
+  },
+];
+
+const questionElement = document.getElementById("question");
+const answerButtons = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+let currentQuestionIndex = 0;
+let score = 0;
+
+function startQuiz() {
+  currentQuestionIndex = 0;
+  score = 0;
+  nextButton.innerHTML = "Next";
+  showQuestion();
+}
+
+function showQuestion() {
+  resetState();
+  let currentQuestion = questions[currentQuestionIndex];
+  let questionNo = currentQuestionIndex + 1;
+
+  questionElement.innerHTML = questionNo + ". " + currentQuestion.question;
+
+  currentQuestion.answers.forEach((answer) => {
+    const button = document.createElement("button");
+    button.innerHTML = answer.text;
+    button.classList.add("btn");
+    answerButtons.appendChild(button);
+    if (answer.correct) {
+      button.dataset.correct = answer.correct;
+    }
+    button.addEventListener("click", selectAnswer);
+  });
+}
+
+function resetState() {
+  nextButton.style.display = "none";
+  while (answerButtons.firstChild) {
+    answerButtons.removeChild(answerButtons.firstChild);
+  }
+}
+
+function selectAnswer(e) {
+  const selectedBtn = e.target;
+  const isCorrect = selectedBtn.dataset.correct === "true";
+  if (isCorrect) {
+    selectedBtn.classList.add("correct");
+    score++;
+  } else {
+    selectedBtn.classList.add("incorrect");
+  }
+  Array.from(answerButtons.children).forEach((button) => {
+    if (button.dataset.correct === "true") {
+      button.classList.add("correct");
+    }
+    button.disabled = true;
+  });
+  nextButton.style.display = "block";
+}
+
+function handleNextQuestion() {
+  currentQuestionIndex++;
+  if (currentQuestionIndex < questions.length) {
+    showQuestion();
+  } else {
+    showScore();
+  }
+}
+
+nextButton.addEventListener("click", () => {
+  if (currentQuestionIndex < questions.length) {
+    handleNextQuestion();
+  } else {
+    startQuiz();
+  }
+});
+function showScore() {
+  resetState();
+  questionElement.innerHTML = `You scored ${score} out of ${questions.length} !`;
+  nextButton.innerHTML = "Play Again";
+  nextButton.style.display = "block";
+}
+
+startQuiz();
